ea1a11d04000d1b62874d4fe8ad16392c8818d17
Robson Florencio <robsoncflorencio@gmail.com>
2019-11-06 05:10:08 -0300

    Add files via upload

---
+++ padlock.ino
@@ -0,0 +1,79 @@
+//This example code is in the Public Domain (or CC0 licensed, at your option.)
+//By Evandro Copercini - 2018
+//
+//This example creates a bridge between Serial and Classical Bluetooth (SPP)
+//and also demonstrate that SerialBT have the same functionalities of a normal Serial
+
+#include "BluetoothSerial.h"
+#include <WiFi.h>
+#include <HTTPClient.h>
+
+#if !defined(CONFIG_BT_ENABLED) || !defined(CONFIG_BLUEDROID_ENABLED)
+#error Bluetooth is not enabled! Please run `make menuconfig` to and enable it
+#endif
+
+const char* ssid = "RafaelArthur";
+const char* password =  "meupia03";
+
+
+BluetoothSerial SerialBT;
+
+void setup() {
+  Serial.begin(115200);
+  SerialBT.begin("ESP32test"); //Bluetooth device name
+  Serial.println("The device started, now you can pair it with bluetooth!");
+    delay(4000);   //Delay needed before calling the WiFi.begin
+ 
+  WiFi.begin(ssid, password); 
+ 
+  while (WiFi.status() != WL_CONNECTED) { //Check for the connection
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+ 
+  Serial.println("Connected to the WiFi network");
+}
+
+void loop() {
+  
+ if(WiFi.status()== WL_CONNECTED){
+  if (Serial.available()) {
+    int dadosBluetooth = Serial.read();
+    SerialBT.write(dadosBluetooth);
+  }
+  if (SerialBT.available()) {
+    Serial.write(SerialBT.read());
+  }
+
+   
+   HTTPClient http;   
+ 
+   http.begin("http://jsonplaceholder.typicode.com/posts");  //Specify destination for HTTP request
+   http.addHeader("Content-Type", "text/plain");             //Specify content-type header
+ 
+   int httpResponseCode = http.POST("POSTING from ESP32");   //Send the actual POST request
+ 
+   if(httpResponseCode>0){
+ 
+    String response = http.getString();                       //Get the response to the request
+ 
+    Serial.println(httpResponseCode);   //Print return code
+    Serial.println(response);           //Print request answer
+ 
+   }else{
+ 
+    Serial.print("Error on sending POST: ");
+    Serial.println(httpResponseCode);
+ 
+   }
+ 
+   http.end();  //Free resources
+ 
+ }else{
+ 
+    Serial.println("Error in WiFi connection");   
+ 
+ }
+
+  delay(10000);
+}
